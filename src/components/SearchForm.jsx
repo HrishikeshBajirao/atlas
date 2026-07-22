de8827997935d556275e049b2bd017f0dc557d3e
HrishikeshBajirao <65894264+HrishikeshBajirao@users.noreply.github.com>
2026-07-22 17:11:31 +0530

    fix:added if condition to check if foundCountry is undefined or not before setting recentSearches value using setter function

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -13,12 +13,15 @@ export default function SearchFormContainer({countryInput, searched, setSearched
     try {
       const foundCountry = await getCountry(countryInput.toLowerCase().trim());
       setCountryInfo(foundCountry);
-      setRecentSearches((currentRecentSearches) => [
-        { name: foundCountry.names.common, code: foundCountry.codes.alpha_2 },
-        ...currentRecentSearches
-          .filter((item) => item.name !== foundCountry.names.common)
-          .slice(0, 4),
-      ]); 
+      console.log(foundCountry);
+      if(foundCountry){
+        setRecentSearches((currentRecentSearches) => [
+          { name: foundCountry.names.common, code: foundCountry.codes.alpha_2 },
+          ...currentRecentSearches
+            .filter((item) => item.name !== foundCountry.names.common)
+            .slice(0, 4),
+        ]);
+      } 
     } catch (err) {
       console.error(err);
     } finally {
